6fc0c1b14be74b997ecc8aece235a8f8fa3aa643
YasmimVieira <vieiray937@gmail.com>
2020-08-11 02:12:07 -0300

    fix: continue form component

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -23,4 +23,27 @@ export class TaskService {
   getAll() {
     return this.tasks;
   }
+
+  getById(id: number) {
+    const task = this.tasks.find((value) => value.id == id);
+    return task;
+  }
+
+  save(task: Task) {
+    if (task.id) {
+      const taskArr = this.getById(task.id);
+      taskArr.description = task.description;
+      taskArr.completed = task.completed;
+    } else {
+      const lastId = this.tasks[this.tasks.length-1].id;
+      task.id = lastId + 1;
+      task.completed = false;
+      this.tasks.push(task);
+    }
+  }
+
+  delete(id: number) {
+    const taskIndex = this.tasks.findIndex((value) => value.id == id);
+    this.tasks.splice(taskIndex, 1);
+  }
 }
--- src/app/tasks/task-list-item/task-list-item.component.ts
+++ src/app/tasks/task-list-item/task-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Task } from './../shared/task';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-list-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list-item.component.css']
 })
 export class TaskListItemComponent implements OnInit {
+  @Input()
+  task: Task;
 
   constructor() { }
 
