c500d5d273c951cc9592309313175e86d7ed532e
YasmimVieira <vieiray937@gmail.com>
2020-08-11 18:36:23 -0300

    chore: add logout in navbar

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../account/shared/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private accountServices: AccountService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.accountServices.login('');
+    this.router.navigate(['login'])
   }
 
 }
